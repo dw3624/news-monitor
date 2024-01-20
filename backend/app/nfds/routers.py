bdca130daa7c4a7fbc58febb8803948d333b1999
dongwon kang <dw3624@gmail.com>
2024-01-21 02:20:16 +0900

    feat: modify cfds post parameter

--- backend/app/nfds/routers.py
+++ backend/app/nfds/routers.py
@@ -11,6 +11,32 @@ today = datetime.now().strftime("%Y%m%d")
 
 
 @router.post("/line-fire")
+async def line_fire_detail(kakao_request: dict):
+    """
+    Args:
+        kakao_request (dict): 카카오 요청 json
+    """
+
+    try:
+        line_name = kakao_request["action"]["params"]["lineFire"]
+        fire_msg = scrape_nfds(line_name)
+        response = {
+            "version": "2.0",
+            "template": {"outputs": [{"simpleText": {"text": fire_msg}}]},
+        }
+        return JSONResponse(response)
+
+    except Exception as e:
+        print(repr(e))
+        err_msg = "예기치 못한 오류가 발생했습니다."
+        response = {
+            "version": "2.0",
+            "template": {"outputs": [{"simpleText": {"text": err_msg}}]},
+        }
+        return JSONResponse(response)
+
+
+@router.post("/line-fire/test")
 async def line_fire_detail(line_name: str = Form(...)):
     """
     Args:
