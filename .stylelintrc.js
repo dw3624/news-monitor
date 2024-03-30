d071c582bbdc5c347e538ce97cd63ea2e965dc6a
dongwon kang <dw3624@gmail.com>
2024-03-30 18:02:31 +0900

    feat: project setup

---
+++ .stylelintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-css-modules',
+    'stylelint-config-recess-order',
+    'stylelint-config-tailwindcss',
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    'selector-pseudo-element-colon-notation': 'double',
+    'string-quotes': 'single',
+    'order/order': ['custom-properties', 'declarations'],
+  },
+  ignoreFiles: ['**/node_modules/**'],
+}
